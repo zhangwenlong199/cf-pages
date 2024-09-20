500805ddf7c7f3998f89155aa7672b923af93cf7
zhangwenlong199 <33951798+zhangwenlong199@users.noreply.github.com>
2024-09-20 10:14:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'veigar_3310';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
